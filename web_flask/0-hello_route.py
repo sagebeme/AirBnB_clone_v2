144a6b211d8f9c593e919149b378c3a00ad3f3e0
sagebeme <eudicas5k@gmail.com>
2023-04-23 14:24:09 +0300

    Python Decoration

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,9 +7,8 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-
-
 def hello_hbnb():
     """Prints Hello HBNB! when (/) is called"""
     return 'Hello HBNB!'
