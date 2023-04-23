f7db61183c02e3b27b47c52bd41b3d1bb721ad47
sagebeme <eudicas5k@gmail.com>
2023-04-23 14:10:41 +0300

    pycodestyle and doc string

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,14 +8,17 @@ from flask import Flask
 app = Flask(__name__)
 
 app.route("/", strict_slashes=False)
-"""
-Prints Hello HBNB! when (/) is called
-"""
+
+
 def hello():
+    """
+    Prints Hello HBNB! when (/) is called
+    """
     return 'Hello HBNB!'
+
 
 if (__name__) == "__main__":
     """
     Main function getting called
     """
-    app.run(host="0.0.0.0",port = 5000)
+    app.run(host="0.0.0.0", port=5000)
